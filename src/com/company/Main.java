c11abe86f0c17a11e38e25e990f436842cbecc32
serkanbal <serkanbaltr@gmail.com>
2016-10-09 12:49:41 -0400

    Added the countdown time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
+
 public class Main {
+    static Thread thr = new Thread();
 
     public static void main(String[] args) {
-    System.out.println(shakeTheBall());
+    countDownTimer();
     }
 
     public static int getRandomNumber() {
-        int rng0 = 0 + (int)(Math.random() * ((8 - 0) + 1));
+        int rng0 = 0 + (int) (Math.random() * ((8 - 0) + 1));
         return rng0;
     }
 
@@ -46,4 +50,26 @@ public class Main {
         return computerResponse;
     }
 
+    public static void shakeAgain() {
+        System.out.println("Do you want to play again? (Type in yes / no)");
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        if (userInput.toUpperCase().contains("YES") == true ||  userInput.toUpperCase().contains("Y") == true) {
+            System.out.println("LOL");
+        } else if (userInput.toUpperCase().contains("NO") == true ||  userInput.toUpperCase().contains("N") == true) {
+            System.out.println("OLO");
+        }
+
+    }
+
+    public static void countDownTimer() {
+        try {
+            for (int i = 3; i >= 0; i--) {
+                thr.sleep(1000);
+                System.out.println(i + "..");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();}
+    }
 }
+
